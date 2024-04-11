3a45293f9965749697b62a4915ef24b3d8faa7e9
RenatoEnciso <renatoenciso22@gmail.com>
2024-04-10 20:30:11 -0500

    estilos

--- resources/views/campo/create.blade.php
+++ resources/views/campo/create.blade.php
@@ -4,6 +4,7 @@
 @section('title', 'Formulario de Conexión')
 
 @section('content')
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 <link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" />
 <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
 
@@ -215,14 +216,19 @@
                             </div>
                             <br>
                         <button type="submit" class="btn btn-primary">analizar</button>
+                        <a href="{{ route('campo.cancelar')}}" class="btn btn-danger">Cancelar</a>
                     </form>
-                    <a href="{{ route('campo.cancelar')}}" >Cancelar</a>
+                 
                    
                    
                      
                  
     </div>
 </div>
+
+
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js" integrity="sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy" crossorigin="anonymous"></script>
 
 <script>
     
--- resources/views/campo/edit.blade.php
+++ resources/views/campo/edit.blade.php
@@ -4,9 +4,9 @@
 @section('title', 'Formulario de Conexión')
 
 @section('content')
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 <link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" />
 <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
-
 <div class="container mt-5">
     <div class="row">
        
@@ -178,12 +178,18 @@
                             </div>
                             <br>
                         <button type="submit" class="btn btn-primary">analizar</button>
+                        <a href="{{ route('campo.cancelar')}}" class="btn btn-danger">Cancelar</a>
                     </form>
                    
                      
                  
     </div>
 </div>
+
+
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js" integrity="sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy" crossorigin="anonymous"></script>
+
 
 <script>
       const columnas = <?php echo json_encode($columnas); ?>;
