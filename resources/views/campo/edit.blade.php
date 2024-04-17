7a6c44434f7ae42ed83d8367ba6955c2e05e45cb
RenatoEnciso <renatoenciso22@gmail.com>
2024-04-16 19:13:45 -0500

    hola

--- resources/views/campo/edit.blade.php
+++ resources/views/campo/edit.blade.php
@@ -198,13 +198,27 @@
     </div>
 </div>
 
-<link href="https://cdn.jsdelivr.net/npm/tom-select@2.3.1/dist/css/tom-select.css" rel="stylesheet">
-<script src="https://cdn.jsdelivr.net/npm/tom-select@2.3.1/dist/js/tom-select.complete.min.js"></script>
 <link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" />
 <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js" integrity="sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy" crossorigin="anonymous"></script>
 <script src="https://code.jquery.com/jquery-3.6.3.min.js"></script> 
 <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
+
+{{-- <script src="path/to/select2.min.js"></script> --}}
+
+
+{{-- <script>
+  function mensaje() {
+ var $disabledResults = $(".select2");
+  $disabledResults.select2();
+  // $('#idLibro').select2();
+  // $('#idFolio').select2();
+  }
+  setTimeout(mensaje,10000);
+</script> --}}
+
+<link href="https://cdn.jsdelivr.net/npm/tom-select@2.3.1/dist/css/tom-select.css" rel="stylesheet">
+<script src="https://cdn.jsdelivr.net/npm/tom-select@2.3.1/dist/js/tom-select.complete.min.js"></script>
 
 {{-- <script src="path/to/select2.min.js"></script> --}}
 
